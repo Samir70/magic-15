75e743c576839ede0c96059b0b28da607ddab4cf
Samir70home <sbetmouni@talktalk.net>
2019-04-19 16:40:23 +0100

    Now handles wins involving 3 out of 4 or 5 cards

--- src/CompMoves/CompMoves.js
+++ src/CompMoves/CompMoves.js
@@ -4,15 +4,31 @@ function RandomMove (choiceOfCards) {
 
 const noWinner = ["no-one", 0, 0, 0];
 
+function sumThreeAndList (cards, indices) {
+    var cardSubset = cards.filter((c, i) => indices.includes(i));
+    return [cardSubset.reduce((a, b) => a+b), ...cardSubset];
+}
+
+const threeOfFour = [ [0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3] ];
+const threeOfFive = [ 
+    [0, 1, 2], [0, 1, 3], [0, 1, 4], [0, 2, 3], [0, 2, 4],
+    [0, 3, 4], [1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4] 
+]
+
+
 function CheckForWin (player, cards) {
+    var sumList = [];
     switch (cards.length) {
-        case 3 : {
-            if (cards.reduce((a, b) => a+b) === 15) {return [player, ...cards]}
-              else {return noWinner}
-            break;
-        }
+        case 3 : { sumList.push(sumThreeAndList(cards, [0,1,2])); break }
+        case 4 : { sumList = threeOfFour.map(a => sumThreeAndList(cards, a)); break}        
+        case 5 : { sumList = threeOfFive.map(a => sumThreeAndList(cards, a)); break}        
         default : {return noWinner}
     }
+    console.log("this is sumlist: ", sumList)
+    sumList = sumList.filter(s => s[0] === 15);
+    console.log("this is sumlist after filtering: ", sumList)
+    if (sumList.length > 0) { return [player, ...sumList[0].slice(1)]}
+          else {return noWinner};
 }
 
 export {CheckForWin, RandomMove, noWinner};

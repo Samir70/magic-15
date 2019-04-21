5f00333dbdc549e312dc5701e9ee5e2be688766f
Samir70home <sbetmouni@talktalk.net>
2019-04-21 12:27:50 +0100

    Updates

---
+++ precache-manifest.43782a6f9527050ad6561397cce5fd60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00e074b7d9e0689318f",
+    "url": "/magic-15/static/js/runtime~main.36e1bbf6.js"
+  },
+  {
+    "revision": "b27558705d754a5491c5",
+    "url": "/magic-15/static/js/main.0dcf7637.chunk.js"
+  },
+  {
+    "revision": "a419672e0ec5afc7509a",
+    "url": "/magic-15/static/js/2.cd409a5f.chunk.js"
+  },
+  {
+    "revision": "b27558705d754a5491c5",
+    "url": "/magic-15/static/css/main.54f0fe3f.chunk.css"
+  },
+  {
+    "revision": "3068bffa5c581cbfbe2b5a091112b0d3",
+    "url": "/magic-15/index.html"
+  }
+];
